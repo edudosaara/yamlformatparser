f6fc0de10dc15e4164f3bc4a753629e6f05437f4
Eduardo dos Santos Araujo <eduardo.araujo.tj@@gmail.com>
2023-03-08 14:50:48 +0000

    added missing build file

--- build/interfaceGenerator.js
+++ build/interfaceGenerator.js
@@ -1,8 +1,22 @@
 import { createWriteStream } from "fs";
 import path from "path";
 export const firstToUpper = (text) => text[0].toUpperCase() + text.substring(1);
+const formatType = (typeName) => {
+    switch (typeName) {
+        case "integer":
+            return "int";
+        case "integer[]":
+            return "int[]";
+        case "float":
+            return "double";
+        case "float[]":
+            return "double[]";
+        default:
+            return typeName;
+    }
+};
 export const generateCSharpInterface = (data) => {
-    const interfaceName = `I${path.basename(process.cwd())}`;
+    const interfaceName = `I${firstToUpper(path.basename(process.cwd()))}`;
     const fileName = `${interfaceName}.cs`;
     const stream = createWriteStream(fileName);
     stream.once('open', (fd) => {
@@ -10,9 +24,9 @@ export const generateCSharpInterface = (data) => {
             stream.write(`public interface ${interf === "files" ? interfaceName : interf}\n`);
             stream.write("{\n");
             Object.keys(data[interf]).forEach((key) => {
-                stream.write(`\t${data[interf][key].type}${data[interf][key].required ? "" : "?"} ${firstToUpper(key)}` + " { get; set; }\n");
+                stream.write(`    ${formatType(data[interf][key].type)}${data[interf][key].required ? "" : "?"} ${firstToUpper(key)}` + " { get; set; }\n");
             });
-            stream.write("}\n\n");
+            stream.write("}\n");
         });
         stream.end();
     });
